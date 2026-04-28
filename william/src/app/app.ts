4f876fed1511d51c9e7967f225368df91e74dba2
Sylvain Cressenville <sylcressenville@gmail.com>
2026-04-28 11:23:10 +0200

    feat: add footer

--- william/src/app/app.ts
+++ william/src/app/app.ts
@@ -2,12 +2,13 @@ import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {Navbar} from "./navbar/navbar";
 import {Toasters} from './toasters/toasters'
+import {Footer} from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, RouterOutlet, Toasters],
+  imports: [Navbar, RouterOutlet, Toasters, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('william');
