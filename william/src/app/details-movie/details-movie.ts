37ff6a35cddc4753430e393d94e15425b57bc434
UzairGL <theogille06@gmail.com>
2026-04-28 13:50:22 +0200

    Add forms validators

--- william/src/app/details-movie/details-movie.ts
+++ william/src/app/details-movie/details-movie.ts
@@ -4,13 +4,13 @@ import {MoviesApiService} from '../services/movies-api.service';
 import {ReviewApi} from '../services/review-api.service';
 import {Movie} from '../models/movie';
 import {Review} from '../models/review';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {DatePipe} from '@angular/common';
 import { UserService } from '../services/user-service';
 
 @Component({
   selector: 'app-details-movie',
-  imports: [FormsModule, DatePipe],
+  imports: [FormsModule, DatePipe, ReactiveFormsModule],
   templateUrl: './details-movie.html',
   styleUrl: './details-movie.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,12 +21,17 @@ export class DetailsMovie implements OnInit {
   private readonly reviewApi = inject(ReviewApi);
   private readonly userService: UserService = inject(UserService);
 
+  private fb = inject(FormBuilder);
+
   readonly movieId = Number(this.route.snapshot.paramMap.get('id'));
 
   movie = signal<Movie | null>(null);
   reviews = signal<Review[]>([]);
-  rate = signal<number>(5);
-  text = signal('');
+
+  reviewForm = this.fb.group({
+    rate: [5, [Validators.required, Validators.min(0), Validators.max(5)]],
+    text: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]]
+  });
 
   ngOnInit(): void {
     if (!this.movieId) {
@@ -56,34 +61,29 @@ export class DetailsMovie implements OnInit {
     return rate.toFixed(2);
   }
 
-  submitReview(event: SubmitEvent): void {
-    event.preventDefault();
+  submitReview(): void {
+    if (this.reviewForm.invalid) {
+      this.reviewForm.markAllAsTouched();
+      return;
+    }
 
     const movie = this.movie();
     const user = this.userService.getUserfromLS();
 
-    if (!movie || !movie.id || user === undefined) {
-      return;
-    }
+    if (!movie || !user) return;
+
+    const formValues = this.reviewForm.value;
 
     const review: Review = {
       movie,
-      user: {
-        id: user.id,
-        firstName: '',
-        lastName: '',
-        age: 0,
-        email: '',
-        points: 0,
-      },
-      rate: this.rate(),
-      text: this.text(),
+      user: { ...user, firstName: '', lastName: '', age: 0, email: '', points: 0 },
+      rate: formValues.rate!,
+      text: formValues.text!,
       reviewDate: new Date(),
     };
 
     this.reviewApi.addReview(review).subscribe(() => {
-      this.text.set('');
-      this.rate.set(5);
+      this.reviewForm.reset({ rate: 5, text: '' });
       this.loadReviews();
     });
   }
